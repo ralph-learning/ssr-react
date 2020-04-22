35c47742e0586803c4b444b4e373a4018acdebe5
Ralph Effting <ralfting@gmail.com>
2020-04-22 07:14:40 -0300

    Add babel setup

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
-const express = require('express');
+import express from  'express';
 const app = new express();
 
-app.get('/', (_req, res) => {
+app.get('/', async (_req, res) => {
   res.send(`<h1>React!</h1>`);
 });
 
